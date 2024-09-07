4c0a9a27bffa8eb6a8850c4a036647e219ecb3a0
PatrickFrantzen <111250196+PatrickFrantzen@users.noreply.github.com>
2024-09-07 20:54:19 +0200

    update to 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { HTTP_INTERCEPTORS, provideHttpClient,  withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TokenInterceptor } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
